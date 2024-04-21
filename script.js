6cc8d7b90fc72b55ce643fa9c2d3a7d10f241774
Abhishek Khadgi <109225495+evoAB@users.noreply.github.com>
2024-04-21 17:52:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,22 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	let totalcost=0;
+	arr.sort((a,b)=>a-b);
+
+	while (arr.length>1) {
+		const num1=arr.shift();
+		const num2=arr.shift();
+
+		const cost = num1+num2;
+
+		totalcost+=cost;
+
+		arr.push(cost);
+
+		arr.sort((a, b) => a - b);
+	}
+	return totalcost; 
 }
 
 module.exports=mincost;
